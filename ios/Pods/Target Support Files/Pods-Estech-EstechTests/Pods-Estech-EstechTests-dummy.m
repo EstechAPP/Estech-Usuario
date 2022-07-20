0d7ef5a89210a3746b25de3b8323ddab93343b6f
Matheus Pereira <65426315+MatheusHCP@users.noreply.github.com>
2022-07-19 22:32:34 -0300

    Initial Commit

---
+++ ios/Pods/Target Support Files/Pods-Estech-EstechTests/Pods-Estech-EstechTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Estech_EstechTests : NSObject
+@end
+@implementation PodsDummy_Pods_Estech_EstechTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-Estech/Pods-Estech-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Estech : NSObject
+@end
+@implementation PodsDummy_Pods_Estech
+@end
